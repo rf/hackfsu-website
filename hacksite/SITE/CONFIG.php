d05d38819d2e5a9a60d546ef9fddd181765359b1
Ryan Tucker <ryantucker0@gmail.com>
2014-02-05 16:05:15 -0500

    Initial site files with little changes - will be working on this soon.

---
+++ hacksite/SITE/CONFIG.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * DryThemes
+ *
+ * @author		DryThemes
+ * @copyright   Copyright (c) 2012, DryThemes.
+ * @link		www.drythemes.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+/*
+  | -------------------------------------------------------------------
+  | NOVO CONFIG
+  | -------------------------------------------------------------------
+  | Please fill your site configation details here.
+  | CHANGE ONLY THIS FEW LINE BELLOW
+*/
+
+//SITE GLOBAL CONFIGURATION
+$email = "dzank.mail@yahoo.com";   //<-- Your email
+
+?>
