6180b2217009f4113ec811127f7636d8295e9825
ryantucker0 <ryantucker0@gmail.com>
2014-02-05 20:19:47 -0500

    Testing for different screen sizes now.

--- hacksite/SITE/js/main.js
+++ hacksite/SITE/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
 //Home Background slider
 jQuery.supersized({	
 slide_interval          :   3000,		// Length between transitions
-transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+transition              :   0, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
 transition_speed		:	700,		// Speed of transition				
 slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
 slides 					:  	[			// Slideshow Images
